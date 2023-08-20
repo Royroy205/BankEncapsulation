41c697abb604a2cfe86ecd286d2557a6e56583e1
Royroy205 <leroyjr2344@live.com>
2023-08-19 20:01:58 -0500

    Exercise Complete

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private  double _balance = 0;
+
+        public void deposit(double amount) 
+        {
+
+            _balance += amount;
+        
+        }
+        public double GetBalance()
+        {
+            return _balance;
+        }
+            
+        
+        
+        
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+
+
+
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,63 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+            Console.WriteLine("Welcome to Bank Of Daylight Saves ATM!.....");
+            
+
+           
+
+
+            while (true) 
+            { 
+              
+
+              Console.WriteLine("1: Deposit money");
+              Console.WriteLine("2: Check Balance");
+              Console.WriteLine("3: Exit"); 
+              
+                int option;
+                if(!int.TryParse(Console.ReadLine(), out option)) 
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid option.");
+                    continue;
+
+                    
+                        
+                        
+                }
+
+                switch (option) 
+                {
+                    case 1:
+                        Console.WriteLine("Enter the amount you want to deposit: ");
+                        double amount;
+                        if (!double.TryParse(Console.ReadLine(), out amount)) 
+                        {
+                            Console.WriteLine("Invaild input. Please enter vaild amount");
+                            continue;
+                        }
+                           
+                            account.deposit(amount);
+
+                            Thread.Sleep(1000);
+                            Console.WriteLine("You have deposit");
+                            break;
+                    case 2:
+                            Console.WriteLine($"Account Balance: {account.GetBalance():C}");
+                            break;
+                    case 3:
+                        Console.WriteLine("Thank You!!!");
+                        return;
+                    default:
+                        Console.WriteLine("Invaild option. Please enter a vaild option. ");
+                        break;
+                }
+            }
+
+
+
         }
     }
 }
